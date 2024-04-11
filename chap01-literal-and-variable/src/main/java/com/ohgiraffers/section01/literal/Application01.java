588b2e1fb5be548a704caba643516f7827ce3136
wsy <wsy9088@gmail.com>
2024-04-11 18:02:07 +0900

    first commmit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+    public static void main(String[] args){
+
+        // System -> 패키지 시스템 . -> 참조 연산자 () -> 메소드 값
+        System.out.println(123); // -> 정수형 리터럴
+
+        System.out.println(0.1232132132132132); // -> 실수형 리터럴
+
+        System.out.println('a'); // -> 문자형 리터럴
+
+        System.out.println("hello world"); // -> 문자열형 리터럴
+
+        System.out.println("12345");
+
+        // 논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,28 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+    public static void main(String[] args){
+        // 2 + 2
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+
+        //123 % 10
+        System.out.println(123 % 10);
+
+        // 1.23 + 1.23
+        // 1.23 - 1.23
+        // 1.23 * 1.23
+        // 1.23 / 1.23
+        // 1.23 % 1.0
+
+        System.out.println(1.23 + 1.23);
+        System.out.println(1.23 - 1.23);
+        System.out.println(1.23 / 1.23);
+        System.out.println(1.23 % 1.0);
+
+        // "123" + "12"
+        System.out.println("123" + "12");
+    }
+}
